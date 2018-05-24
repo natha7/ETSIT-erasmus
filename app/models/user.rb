80e639ffefd8180a05545ac1cdb1819d6cd83574
abenitoc <vermibeto@gmail.com>
2018-05-24 21:09:00 +0200

    Added controllers for devise and structure with paperclip and others

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :student_application_form
+
+  has_attached_file :motivation_letter
+  has_attached_file :curriculum_vitae
+  has_attached_file :transcript_of_records
+  has_attached_file :learning_agreement
+
+  def set_default_role
+    self.role ||= :user
+  end
+  
 end
