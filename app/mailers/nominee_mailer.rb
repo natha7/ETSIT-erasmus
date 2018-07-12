6da1a467943516e2c30879de5bfd0e28768abe0f
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2018-07-12 12:55:40 +0200

    Small fix

--- app/mailers/nominee_mailer.rb
+++ app/mailers/nominee_mailer.rb
@@ -3,7 +3,7 @@ class NomineeMailer < ApplicationMailer
 
 	def user_creation_email(nominee, url)
 		@nominee = nominee
-		@nominee_url = url + "/"+ @nominee.registration_token
+		@nominee_url = url + "/register/"+ @nominee.registration_token
 		mail(to: @nominee.email, subject: 'You are nominated to ETSIT-UPM Erasmus Programme', encrypt: true)
 	end
 end
