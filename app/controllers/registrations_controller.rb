9d5c3e6b678ef1a6eb4935f1caf5ccbe8c3b2ff5
abenitoc <bertocode@gmail.com>
2018-08-29 12:28:02 +0200

    Registrations after sign path update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -63,7 +63,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    "/student_application_form/1"
+    config.relative_url_root + "/student_application_form/1"
   end
 
   # The path used after sign up for inactive accounts.
