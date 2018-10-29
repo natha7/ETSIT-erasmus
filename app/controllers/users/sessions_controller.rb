5422e5ad0bc0175255d411f66fc98ef91d6e90a3
abenitoc <bertocode@gmail.com>
2018-10-29 17:13:19 +0100

    Modify headers

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,7 +12,7 @@ class Users::SessionsController < Devise::SessionsController
      #@post_params["postLocationUrl"] = "http://pruebas.etsit.upm.es"
      #@post_params["redirectLocationUrl"] = "http://pruebas.etsit.upm.es"
      @post_params["country"] = "ES"
-     @post_params["sendMethods"] = "POST"
+     #@post_params["sendMethods"] = "POST"
      @login_url = saml_config(idp_entity_id).idp_sso_target_url
      super
   end
