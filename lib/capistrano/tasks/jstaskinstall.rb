5b623e19760df1a7b6736ef23410376438c4d48c
abenitoc <bertocode@gmail.com>
2018-11-22 10:45:42 +0100

    Update

--- lib/capistrano/tasks/jstaskinstall.rb
+++ lib/capistrano/tasks/jstaskinstall.rb
@@ -1,7 +1,9 @@
-desc "Install saml2-node libraries"
 
-task :saml2_node_libraries do
-  Terrapin::CommandLine.new("npm install;")
+namespace :deploy do
+  desc "Install saml2-node libraries"
+  task :saml2_node_libraries do
+    Terrapin::CommandLine.new("npm install;")
+  end
+
+  after("deploy:cleanup", "saml2_node_libraries")
 end
-
-after("deploy:cleanup", "saml2_node_libraries")
